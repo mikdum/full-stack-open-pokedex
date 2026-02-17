d4b6d8cd0585f9dc7e2594d2c8a59e397ecef085
Mikhail Dumma <mikdumma@gmail.com>
2026-02-17 13:01:33 +0300

    11.12-1

---
+++ e2e-tests/mytest.spec.js
@@ -0,0 +1,12 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('blank test', async () => {
+    expect(1).toBe(1)
+  })
+  // test('front page can be opened', async ({ page }) => {
+  //   await page.goto('http://localhost:5000/')
+  //   await expect(page.getByText('ivysaur')).toBeVisible()
+  //   await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  // })
+})
